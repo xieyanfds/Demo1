ba148d7bdfc00e77a29fa39f521af8515c5128df
xieyan <xy_fds@163.com>
2018-07-12 11:37:29 +0800

    update

---
+++ src/main/java/staticTest/Main.java
@@ -0,0 +1,13 @@
+package staticTest;
+
+/**
+ * @author xieyan
+ * @description
+ * @date 2018/6/27.
+ */
+public class Main {
+    public static void main(String[] args) {
+        Action2 action2 = new Action2();
+        action2.test();
+    }
+}
